c1d3de9a5467b4bf6ac5f5447884c2dd34c27d45
Kims <sungkyung521@naver.com>
2023-01-12 14:56:02 +0900

    202301121455

--- components/header.js
+++ components/header.js
@@ -6,7 +6,7 @@ export default function Header() {
     <>
       <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <Link legacyBehavior href="/">
+          <Link href="/">
             <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -20,27 +20,28 @@ export default function Header() {
               >
                 <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
               </svg>
-              <span className="ml-3 text-xl">성갱's Development Story</span>
+              <span className="ml-3 text-xl">성경의 개발 노트</span>
             </a>
           </Link>
+
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <Link legacyBehavior href="/">
+            <Link href="/">
               <a className="mr-5 hover:text-gray-900">Home</a>
             </Link>
 
-            <Link legacyBehavior href="/projects">
+            <Link href="/projects">
               <a className="mr-5 hover:text-gray-900">Project</a>
             </Link>
-                        
+
             <a
               href="https://open.kakao.com/o/s91Oe0Xe"
               className="mr-5 hover:text-gray-900"
             >
               OpenTalk
             </a>
-          </nav>     
-          {/*다크모드 토글 버튼 작업해야 함 */}     
-          <DarkModeToggleButton/>
+          </nav>
+          {/* 다크모드 토글 버튼 작업해야함 */}
+          <DarkModeToggleButton />
         </div>
       </header>
     </>
