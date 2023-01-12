04d372a4c28e217ef08d74facd15fc13f74cc171
Kims <sungkyung521@naver.com>
2023-01-12 14:59:16 +0900

    header update

--- components/header.js
+++ components/header.js
@@ -6,7 +6,7 @@ export default function Header() {
     <>
       <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <Link href="/">
+          <Link legacyBehavior href="/">
             <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -25,11 +25,11 @@ export default function Header() {
           </Link>
 
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <Link href="/">
+            <Link legacyBehavior href="/">
               <a className="mr-5 hover:text-gray-900">Home</a>
             </Link>
 
-            <Link href="/projects">
+            <Link legacyBehavior href="/projects">
               <a className="mr-5 hover:text-gray-900">Project</a>
             </Link>
 
