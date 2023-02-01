aa4d30c01449c1e0118a502c1b49e18fc839159a
Kims <sungkyung521@naver.com>
2023-02-01 16:03:27 +0900

    20230201 v1.4 upload

--- components/home/hero.js
+++ components/home/hero.js
@@ -17,18 +17,18 @@ export default function Hero() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="w-10 h-10 text-white p-2 bg-purple-500 rounded-full"
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+            <path 
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
             />
           </svg>
         </div>
-        <p className="mb-8 leading-relaxed">
+        <div className="mb-8 leading-relaxed">
           <h2>Application Developer</h2>
           <a>안녕하세요! 꾸준히 성장하는 개발자 김성경 입니다.</a>
           <br />
@@ -39,8 +39,8 @@ export default function Hero() {
           <a>꼼꼼하게 빈 틈을 채워나가는 개발자</a>
           <br />
           <a>고객이 필요로 하는 것이 무엇인지 고민하는 개발자</a>
-          <br />
-        </p>
+          <br />     
+        </div>
         {/*Application Developer END*/}
 
         {/* 02. Stack START*/}
@@ -49,30 +49,77 @@ export default function Hero() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="w-10 h-10 text-white p-2 bg-red-700 rounded-full align-self: center"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25"
             />
           </svg>
         </div>
-        <p className="mb-8 leading-relaxed">
+        <div className="mb-8 leading-relaxed">
           <h2>Stack</h2>
 
           <h2>다룰 수 있는 기술/언어들 입니다.</h2>
-          <a>MFC C++ Java C# VB</a>
-          <br />
-          <a>Oracle SQL PostgreSQL</a>
-          <br />
-          <a>HTML CSS JavaScript React Next.js </a>
-          <br />
-          <a>Git OA</a>
-          <br />
-        </p>
+          <div className="pb-4 pt-4">
+            <a className="px-2 py-1 mr-2 rounded-md bg-red-300 dark:bg-red-300 w-30">
+              MFC
+            </a>
+            <a className="px-2 py-1 mr-2 rounded-md bg-red-500 dark:bg-red-500 w-30">
+              C++
+            </a>
+            <a className="px-2 py-1 mr-2 rounded-md bg-fuchsia-600 dark:bg-fuchsia-600 w-30">
+              Java
+            </a>
+            <a className="px-2 py-1 mr-2 rounded-md bg-cyan-500 dark:bg-cyan-500 w-30">
+              C#
+            </a>
+            <a className="px-2 py-1 mr-2 rounded-md bg-indigo-600 dark:bg-indigo-600 w-30">
+              VB
+            </a>
+            <br />
+          </div>
+
+          <div className="pb-4">
+            <a className="px-2 py-1 mr-2 rounded-md bg-green-500 dark:bg-green-500 w-30">
+              Oracle SQL
+            </a>
+            <a className="px-2 py-1 mr-2 rounded-md bg-sky-400 dark:bg-sky-400 w-30">
+              PostgreSQL
+            </a>
+            <br />
+          </div>
+          <div className="pb-4">
+            <a className="px-2 py-1 mr-2 rounded-md bg-lime-600 dark:bg-lime-600 w-30">
+              HTML
+            </a>
+            <a className="px-2 py-1 mr-2 rounded-md bg-yellow-400 dark:bg-yellow-400 w-30">
+              CSS
+            </a>
+            <a className="px-2 py-1 mr-2 rounded-md bg-violet-700 dark:bg-violet-700 w-30">
+              JavaScript
+            </a>
+            <a className="px-2 py-1 mr-2 rounded-md bg-blue-700 dark:bg-blue-700 w-30">
+              React
+            </a>
+            <a className="px-2 py-1 mr-2 rounded-md bg-neutral-500 dark:bg-neutral-500 w-30">
+              Next.js
+            </a>
+            <br />
+          </div>
+          <div className="pb-4">
+            <a className="px-2 py-1 mr-2 rounded-md bg-purple-500 dark:bg-purple-500 w-30">
+              Git
+            </a>
+            <a className="px-2 py-1 mr-2 rounded-md bg-red-400 dark:bg-red-400 w-30">
+              OA
+            </a>
+            <br />
+          </div>
+        </div>
         {/* 02. Stack END*/}
 
         {/* 03. 기본인적사항 START*/}
@@ -81,18 +128,18 @@ export default function Hero() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="w-10 h-10 text-white p-2 bg-yellow-500 rounded-full"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
             />
           </svg>
         </div>
-        <p className="mb-8 leading-relaxed">
+        <div className="mb-8 leading-relaxed">
           <h2>기본인적사항</h2>
           <a>김성경 (SungKyung Kim)</a>
           <br />
@@ -102,7 +149,7 @@ export default function Hero() {
           <br />
           <a>010-8711-1631</a>
           <br />
-        </p>
+        </div>
         {/* 03. 기본인적사항 END*/}
 
         {/* 04. 학력 START*/}
@@ -111,18 +158,18 @@ export default function Hero() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="w-10 h-10 text-white p-2 bg-green-500 rounded-full"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"
             />
           </svg>
         </div>
-        <p className="mb-8 leading-relaxed">
+        <div className="mb-8 leading-relaxed">
           <h2>학력</h2>
           <a>
             2016.03 ~ 2017.02 서일대학교 컴포터소프트웨어학과 (전공심화)
@@ -133,7 +180,7 @@ export default function Hero() {
           <br />
           <a>2006.03 ~ 2009.02 덕소고등학교(인문계)</a>
           <br />
-        </p>
+        </div>
         {/* 04. 학력 END*/}
 
         {/* 05. 교육 START*/}
@@ -142,25 +189,25 @@ export default function Hero() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="w-10 h-10 text-white p-2 bg-green-500 rounded-full"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"
             />
           </svg>
         </div>
-        <p className="mb-8 leading-relaxed">
+        <div className="mb-8 leading-relaxed">
           <h2>교육</h2>
           <a>
             2016.10 ~ 2017.04 [NCS] 기업이 요구하느느 자바 웹 개발자 양성과정 /
             KH정보교육원{" "}
           </a>
           <br />
-        </p>
+        </div>
         {/* 05. 교육 END*/}
 
         <div className="flex justify-center">
