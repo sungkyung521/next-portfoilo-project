513c8f427ff2fc875d1f36a2e629f02f36f5086c
Kims <sungkyung521@naver.com>
2023-02-03 14:11:07 +0900

    20230203 v1.1 upload

--- components/home/hero.js
+++ components/home/hero.js
@@ -5,11 +5,11 @@ export default function Hero() {
   return (
     <>
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+       {/*<h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
           내용 써야함
           <br className="hidden lg:inline-block" />
           내용 써야함
-        </h1>
+        </h1> */}
 
         {/* 01. Application Developer START*/}
         <div>
