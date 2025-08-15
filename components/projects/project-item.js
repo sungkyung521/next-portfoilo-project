b9933f4171c427137294434f3ab871f3adb457ae
sungkyung521 <sungkyung521@naver..com>
2025-08-15 11:34:15 +0900

    20250815 CI 문구 작업

--- components/projects/project-item.js
+++ components/projects/project-item.js
@@ -3,7 +3,6 @@ import Image from "next/image";
 export default function ProjectItem({ data }) {
   const title = data.properties.이름.title[0].plain_text;
   const github = data.properties.Github.url;
-  //const youtube = data.properties.Youtube.url
   const description = data.properties.Remark.rich_text[0].plain_text;
   const imgSrc = data.cover.file?.url || data.cover.external.url;
   const tags = data.properties.Tags.multi_select;
@@ -25,45 +24,43 @@ export default function ProjectItem({ data }) {
       endDateStringArray[2]
     );
 
-    console.log(`startDate: ${startDate}`);
-    console.log(`endDate: ${endDate}`);
-
     const diffInMs = Math.abs(endDate - startDate);
-    const result = diffInMs / (1000 * 60 * 60 * 24);
-
-    console.log(`기간 : ${result}`);
-    return result;
+    return diffInMs / (1000 * 60 * 60 * 24);
   };
 
   return (
     <div className="project-card">
-      <Image
-        className="rounded-t-xl"
-        src={imgSrc}
-        alt="cover image"
-        width="100"
-        height="50"
-        layout="responsive"
-        objectFit="cover"
-        quality={100}
-      />
+      {/* ✅ 이미지 흰 배경 래퍼 */}
+      <div className="bg-white dark:bg-white p-3 flex items-center justify-center rounded-t-xl">
+        <Image
+          src={imgSrc}
+          alt="cover image"
+          width="100"
+          height="50"
+          layout="responsive"
+          objectFit="cover"
+          quality={100}
+        />
+      </div>
 
       <div className="p-4 flex flex-col">
         <h1 className="text-2xl font-bold">{title}</h1>
         <h3 className="mt-4 text-xl">{description}</h3>
-        <a href={github}>깃허브 바로가기</a>
+        <a href={github} className="text-blue-500 hover:underline">
+          깃허브 바로가기
+        </a>
 
-        <p className="my-1 ">
+        <p className="my-1">
           작업기간 : {start} ~ {end} ({calculatedPeriod(start, end)}일)
         </p>
-        <div className="flex items-start mt-2">
+        <div className="flex items-start mt-2 flex-wrap">
           {tags.map((aTag) => (
-            <h1
-              className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30"
+            <span
+              className="px-2 py-1 mr-2 mb-2 rounded-md bg-sky-200 dark:bg-sky-700 w-fit"
               key={aTag.id}
             >
               {aTag.name}
-            </h1>
+            </span>
           ))}
         </div>
       </div>
